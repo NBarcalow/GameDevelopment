993ce269e84c551679859fdf8a1a3303e28e4a79
NBarcalow <60424656+NBarcalow@users.noreply.github.com>
2025-09-09 22:00:35 -0600

    Stage 4
    Stage 4: Despawn trees when out of sight

--- 2d-project-start/tree.gd
+++ 2d-project-start/tree.gd
@@ -1,1 +1,12 @@
 extends StaticBody2D
+	
+var on_screen = false
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	if on_screen:
+		queue_free()
+
+
+func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
+	on_screen = true
